a65c2b25556b325da0482a00cfe21e0f362b77b2
James Tomlinson <jamest@stripe.com>
2024-04-22 15:00:43 +1000

    updated to use RN Payment Sheet

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import { WebView } from "react-native-webview";
+import React, { useEffect } from "react";
+import { Alert } from "react-native";
+import {
+	initStripe,
+	useStripe,
+	PaymentSheet,
+} from "@stripe/stripe-react-native";
 
 export default function App() {
+	const { initPaymentSheet, presentPaymentSheet } = useStripe();
+
+	useEffect(() => {
+		initStripe({
+			publishableKey:
+				"pk_test_51Me6XAHdJokNZ98tzjQti0Ln3JNqvf8fqnAJ2VPm9oRzmgWEvjnUOZPubjpmSPVGE7bzAQSi5XDOCQkdh9imQit6001p6Hlk04",
+		});
+	}, []);
+
+	const onMessage = async (event) => {
+		const message = JSON.parse(event.nativeEvent.data);
+
+		if (message.action === "make_payment") {
+			// Initialize the payment sheet
+			const { error } = await initPaymentSheet({
+				customerId: "cus_NOumxVF9M1Wi1b",
+				customerEphemeralKeySecret:
+					"ek_test_YWNjdF8xTWU2WEFIZEpva05aOTh0LHl0dGhMWjlZcEU1YVFDdVMyd0dKMGRCd28zbE45U1M_00jwPlkFYy",
+				merchantDisplayName: "Power Co",
+				paymentIntentClientSecret: message.clientSecret,
+				returnURL: "https://betaorigindemo.stripedemos.com/success",
+				googlePay: {
+					merchantCountryCode: "AU",
+					testEnv: true, // use test environment
+				},
+				applePay: true,
+				defaultBillingDetails: {
+					name: "James Tomlinson",
+					email: "james@customer.com",
+					phone: "+61499652416",
+				},
+			});
+			if (!error) {
+				const { error } = await presentPaymentSheet();
+				if (error) {
+					Alert.alert(`Error code: ${error.code}`, error.message);
+				} else {
+					Alert.alert("Success", "The payment was confirmed successfully");
+				}
+			}
+		}
+	};
 	return (
 		<View style={{ flex: 1 }}>
 			<WebView
-				source={{ uri: "https://origindemo.stripedemos.com" }}
+				source={{ uri: "https://betaorigindemo.stripedemos.com" }}
 				style={{ flex: 1 }}
-				enableApplePay={true}
+				// enableApplePay={true}
+				onMessage={onMessage}
 			/>
 			<StatusBar style="auto" />
 		</View>
